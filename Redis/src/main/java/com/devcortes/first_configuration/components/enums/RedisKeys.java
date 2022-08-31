9db4c7cb6b63e1c765ccd43dfdbb1087af2c5675
vsemeniuk <vsemeniuk@atlassian.com>
2022-08-31 17:12:24 +0300

    removed Redis

--- Redis/src/main/java/com/devcortes/first_configuration/components/enums/RedisKeys.java
+++
@@ -1,8 +0,0 @@
-package com.devcortes.first_configuration.components.enums;
-
-/**
- * Created by cortes on 24.11.18.
- */
-public enum RedisKeys {
-	FAMILY, FRIEND, PERSON, USER
-}
--- Redis/src/main/java/com/devcortes/first_configuration/components/queue/MessagePublisher.java
+++
@@ -1,9 +0,0 @@
-package com.devcortes.first_configuration.components.queue;
-
-/**
- * Created by cortes on 25.11.18.
- */
-public interface MessagePublisher {
-
-	void publish(final String message);
-}
